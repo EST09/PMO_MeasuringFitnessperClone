0e6365d6b1e6cc8dbc5d1e853b8605a99d33f97c
Emily Thomas <esthomas09@hotmail.com>
2021-08-03 10:46:00 +0100

    added some comments and removed some hopefully redundant architecture

--- SingleCellSequencing/train_VAE_3D_emily.py
+++ SingleCellSequencing/train_VAE_3D_emily.py
@@ -1,52 +1,106 @@
-#https://keras.io/examples/generative/vae/
-#https://blog.keras.io/building-autoencoders-in-keras.html
+# Taking Saeed's code and converting it to make it more intuitive and commented 
+# Making it run on a clone by clone basis
 
-#Setup
+'''
+## Resources
+'''
+
+# https://blog.keras.io/building-autoencoders-in-keras.html
+# https://keras.io/examples/generative/vae/
+# https://github.com/keras-team/keras-io/blob/master/examples/generative/vae.py
+
+'''
+## Dependancies
+'''
 
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
+import matplotlib.pyplot as plt
+import pickle
+import os
+from os.path import expanduser
+import pandas as pd
+
+from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose, concatenate, BatchNormalization, Activation
+from keras.models import Model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.datasets import mnist
+from keras.callbacks import TensorBoard, ModelCheckpoint
+from keras import backend as K
+
 from tensorflow.keras import layers
+from tensorflow import keras
+import tensorflow as tf
 
-#Create a sampling layer
+from loadData import loadData
 
-class Sampling(layers.Layer):
-    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+'''
+## File paths
+'''
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+home = expanduser("~")
+path2Identities = home + "/smaller_test_imgs" # the file that makes these is miscellaneous.py
+
+#os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" #not sure what this is
+#os.environ["CUDA_VISIBLE_DEVICES"] = "5" 
+
+"""
+## Create a sampling layer
+"""
+
+class Sampling(layers.Layer): 
+    """
+    Uses (z_mean, z_log_var) to sample z, the vector encoding a digit.
+    """
 
     def call(self, inputs):
+        
         z_mean, z_log_var = inputs
         batch = tf.shape(z_mean)[0]
         dim = tf.shape(z_mean)[1]
         epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        
         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
-#Build the encoder
+"""
+## Build the encoder
+"""
 
-latent_dim = 2
+def VAE_encoder(w,h,c,latent_dim):
 
-encoder_inputs = keras.Input(shape=(28, 28, 1))
-x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
-x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
-x = layers.Flatten()(x)
-x = layers.Dense(16, activation="relu")(x)
-z_mean = layers.Dense(latent_dim, name="z_mean")(x)
-z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
-z = Sampling()([z_mean, z_log_var])
-encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
-encoder.summary()
+    encoder_inputs = keras.Input(shape=(w, h, c))
+    x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+    x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(16, activation="relu")(x)
+    z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+    z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+    z = Sampling()([z_mean, z_log_var])
+    encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+    encoder.summary()
 
-#Build the decoder
+    return encoder
 
-latent_inputs = keras.Input(shape=(latent_dim,))
-x = layers.Dense(7 * 7 * 64, activation="relu")(latent_inputs)
-x = layers.Reshape((7, 7, 64))(x)
-x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
-x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
-decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
-decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
-decoder.summary()
+"""
+## Build the decoder
+"""
 
-#Define the VAE as a Model with a custom train_step
+def VAE_decoder(latent_dim):
+
+    latent_inputs = keras.Input(shape=(latent_dim,))
+    x = layers.Dense(75 * 75 * 64, activation="relu")(latent_inputs)
+    x = layers.Reshape((75, 75, 64))(x)
+    x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
+    x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
+    decoder_outputs = layers.Conv2DTranspose(3, 3, activation="sigmoid", padding="same")(x)
+    decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+    decoder.summary()
+
+    return decoder
+
+"""
+## Define the VAE as a `Model` with a custom `train_step`
+"""
 
 class VAE(keras.Model):
     def __init__(self, encoder, decoder, **kwargs):
@@ -89,73 +143,291 @@ class VAE(keras.Model):
             "reconstruction_loss": self.reconstruction_loss_tracker.result(),
             "kl_loss": self.kl_loss_tracker.result(),
         }
+        
+    def call(self, data):
+        [z_mean,z_log_var,z]  = self.encoder(data)
+        y_pred = self.decoder(z)
+        return y_pred
 
-#Train the VAE
+'''
+## Load the Data and Train the VAE
+'''
 
-(x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
-mnist_digits = np.concatenate([x_train, x_test], axis=0)
-mnist_digits = np.expand_dims(mnist_digits, -1).astype("float32") / 255
-
+encoder = VAE_encoder(300,300,3,3)
+decoder = VAE_decoder(3)
 vae = VAE(encoder, decoder)
 vae.compile(optimizer=keras.optimizers.Adam())
-vae.fit(mnist_digits, epochs=30, batch_size=128)
-
-#Display a grid of sampled digits
-
-import matplotlib.pyplot as plt
 
 
-def plot_latent_space(vae, n=30, figsize=15):
-    # display a n*n 2D manifold of digits
-    digit_size = 28
-    scale = 1.0
-    figure = np.zeros((digit_size * n, digit_size * n))
-    # linearly spaced coordinates corresponding to the 2D plot
-    # of digit classes in the latent space
-    grid_x = np.linspace(-scale, scale, n)
-    grid_y = np.linspace(-scale, scale, n)[::-1]
+Identities = os.listdir(path2Identities)
 
-    for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample = np.array([[xi, yi]])
-            x_decoded = vae.decoder.predict(z_sample)
-            digit = x_decoded[0].reshape(digit_size, digit_size)
-            figure[
-                i * digit_size : (i + 1) * digit_size,
-                j * digit_size : (j + 1) * digit_size,
-            ] = digit
+for f,identity in enumerate(Identities):
+    if os.path.exists(os.path.join(current_directory, '../LatentSpaceVAE_3D',identity+'.pickle')):  
+        continue
 
-    plt.figure(figsize=(figsize, figsize))
-    start_range = digit_size // 2
-    end_range = n * digit_size + start_range
-    pixel_range = np.arange(start_range, end_range, digit_size)
-    sample_range_x = np.round(grid_x, 1)
-    sample_range_y = np.round(grid_y, 1)
-    plt.xticks(pixel_range, sample_range_x)
-    plt.yticks(pixel_range, sample_range_y)
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.imshow(figure, cmap="Greys_r")
-    plt.show()
+    else:
+        print('item {} not exists'.format(identity))
+        path2Images = os.path.join(path2Identities,identity)
+        
+        x_train,x_test = loadData(path2Identities,300,300)
+
+        x_train = x_train.astype('float32') / 255.
+        x_test = x_test.astype('float32') / 255.
+
+    datagen = ImageDataGenerator(
+        featurewise_center=False,
+        featurewise_std_normalization=False,
+        rotation_range=90,
+        horizontal_flip=True,
+        vertical_flip=True)
+
+    datagen.fit(x_train)
+
+    print(identity)
+    print('item {} is now running {}'.format(f,identity))
+    
+    print(x_train.shape)
+    print(x_test.shape)
+
+    if (x_train.shape[0] == 0) or (x_test.shape[0] == 0):
+        continue
+    
+    traingen = datagen.flow(x_train,batch_size=32)
+    
+    history = vae.fit(traingen, epochs=10, shuffle=True, validation_data= (x_test,x_test),callbacks=[ModelCheckpoint(os.path.join(current_directory,'../modelsVAE_3D/',identity+'.h5'), monitor='reconstruction_loss', verbose=1, save_best_only=True,save_weights_only=True)], verbose=2)
+
+    # take a look at the reconstructed digits
+    #decoded_imgs = vae.decoder.predict(x_test)
+    #print(decoded_imgs.shape)
+    '''
+    n = 10
+    plt.figure(figsize=(10, 4), dpi=100)
+    for i in range(n):
+        # display original
+        ax = plt.subplot(2, n, i + 1)
+        plt.imshow(x_test[i].reshape(28, 28))
+        plt.gray()
+        ax.set_axis_off()
+
+        # display reconstruction
+        ax = plt.subplot(2, n, i + n + 1)
+        plt.imshow(decoded_imgs[i].reshape(28, 28))
+        plt.gray()
+        ax.set_axis_off()
+
+    plt.save('reconstruction.png')
+    '''
+    # take a look at the 128-dimensional encoded representation
+    # these representations are 8x4x4, so we reshape them to 4x32 in order to be able to display them as grayscale images
+
+    #encoder = Model(input_img, encoded)
+    z_mean, _, _ = vae.encoder.predict(x_test)
+    print(z_mean.shape)
+    
+    pickle.dump(z_mean, open(os.path.join(current_directory, '../LatentSpaceVAE_3D/', identity+'.pickle'), 'wb'))
+    '''
+    n = 10
+    plt.figure(figsize=(10, 4), dpi=100)
+    for i in range(n):
+        ax = plt.subplot(1, n, i + 1)
+        plt.imshow(encoded_imgs[i].reshape(4, 4 * 8).T)
+        plt.gray()
+        ax.set_axis_off()
+
+    plt.savefig('latentSpace.pnd')
+    '''
+    K.clear_session()
 
 
-plot_latent_space(vae)
-
-#Display how the latent space clusters different digit classes
-
-def plot_label_clusters(vae, data, labels):
-    # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = vae.encoder.predict(data)
-    plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=labels)
-    plt.colorbar()
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.show()
 
 
-(x_train, y_train), _ = keras.datasets.mnist.load_data()
-x_train = np.expand_dims(x_train, -1).astype("float32") / 255
 
-plot_label_clusters(vae, x_train, y_train)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #https://keras.io/examples/generative/vae/
+# #https://blog.keras.io/building-autoencoders-in-keras.html
+
+# #Setup
+
+# import numpy as np
+# import tensorflow as tf
+# from tensorflow import keras
+# from tensorflow.keras import layers
+
+# #Create a sampling layer
+
+# class Sampling(layers.Layer):
+#     """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
+
+#     def call(self, inputs):
+#         z_mean, z_log_var = inputs
+#         batch = tf.shape(z_mean)[0]
+#         dim = tf.shape(z_mean)[1]
+#         epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+#         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+
+# #Build the encoder
+
+# latent_dim = 2
+
+# encoder_inputs = keras.Input(shape=(28, 28, 1))
+# x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+# x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+# x = layers.Flatten()(x)
+# x = layers.Dense(16, activation="relu")(x)
+# z_mean = layers.Dense(latent_dim, name="z_mean")(x)
+# z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
+# z = Sampling()([z_mean, z_log_var])
+# encoder = keras.Model(encoder_inputs, [z_mean, z_log_var, z], name="encoder")
+# encoder.summary()
+
+# #Build the decoder
+
+# latent_inputs = keras.Input(shape=(latent_dim,))
+# x = layers.Dense(7 * 7 * 64, activation="relu")(latent_inputs)
+# x = layers.Reshape((7, 7, 64))(x)
+# x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
+# x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
+# decoder_outputs = layers.Conv2DTranspose(1, 3, activation="sigmoid", padding="same")(x)
+# decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
+# decoder.summary()
+
+# #Define the VAE as a Model with a custom train_step
+
+# class VAE(keras.Model):
+#     def __init__(self, encoder, decoder, **kwargs):
+#         super(VAE, self).__init__(**kwargs)
+#         self.encoder = encoder
+#         self.decoder = decoder
+#         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+#         self.reconstruction_loss_tracker = keras.metrics.Mean(
+#             name="reconstruction_loss"
+#         )
+#         self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+
+#     @property
+#     def metrics(self):
+#         return [
+#             self.total_loss_tracker,
+#             self.reconstruction_loss_tracker,
+#             self.kl_loss_tracker,
+#         ]
+
+#     def train_step(self, data):
+#         with tf.GradientTape() as tape:
+#             z_mean, z_log_var, z = self.encoder(data)
+#             reconstruction = self.decoder(z)
+#             reconstruction_loss = tf.reduce_mean(
+#                 tf.reduce_sum(
+#                     keras.losses.binary_crossentropy(data, reconstruction), axis=(1, 2)
+#                 )
+#             )
+#             kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+#             kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+#             total_loss = reconstruction_loss + kl_loss
+#         grads = tape.gradient(total_loss, self.trainable_weights)
+#         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+#         self.total_loss_tracker.update_state(total_loss)
+#         self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+#         self.kl_loss_tracker.update_state(kl_loss)
+#         return {
+#             "loss": self.total_loss_tracker.result(),
+#             "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+#             "kl_loss": self.kl_loss_tracker.result(),
+#         }
+
+# #Train the VAE
+
+# (x_train, _), (x_test, _) = keras.datasets.mnist.load_data()
+# mnist_digits = np.concatenate([x_train, x_test], axis=0)
+# mnist_digits = np.expand_dims(mnist_digits, -1).astype("float32") / 255
+
+# vae = VAE(encoder, decoder)
+# vae.compile(optimizer=keras.optimizers.Adam())
+# vae.fit(mnist_digits, epochs=30, batch_size=128)
+
+# #Display a grid of sampled digits
+
+# import matplotlib.pyplot as plt
+
+
+# def plot_latent_space(vae, n=30, figsize=15):
+#     # display a n*n 2D manifold of digits
+#     digit_size = 28
+#     scale = 1.0
+#     figure = np.zeros((digit_size * n, digit_size * n))
+#     # linearly spaced coordinates corresponding to the 2D plot
+#     # of digit classes in the latent space
+#     grid_x = np.linspace(-scale, scale, n)
+#     grid_y = np.linspace(-scale, scale, n)[::-1]
+
+#     for i, yi in enumerate(grid_y):
+#         for j, xi in enumerate(grid_x):
+#             z_sample = np.array([[xi, yi]])
+#             x_decoded = vae.decoder.predict(z_sample)
+#             digit = x_decoded[0].reshape(digit_size, digit_size)
+#             figure[
+#                 i * digit_size : (i + 1) * digit_size,
+#                 j * digit_size : (j + 1) * digit_size,
+#             ] = digit
+
+#     plt.figure(figsize=(figsize, figsize))
+#     start_range = digit_size // 2
+#     end_range = n * digit_size + start_range
+#     pixel_range = np.arange(start_range, end_range, digit_size)
+#     sample_range_x = np.round(grid_x, 1)
+#     sample_range_y = np.round(grid_y, 1)
+#     plt.xticks(pixel_range, sample_range_x)
+#     plt.yticks(pixel_range, sample_range_y)
+#     plt.xlabel("z[0]")
+#     plt.ylabel("z[1]")
+#     plt.imshow(figure, cmap="Greys_r")
+#     plt.show()
+
+
+# plot_latent_space(vae)
+
+# #Display how the latent space clusters different digit classes
+
+# def plot_label_clusters(vae, data, labels):
+#     # display a 2D plot of the digit classes in the latent space
+#     z_mean, _, _ = vae.encoder.predict(data)
+#     plt.figure(figsize=(12, 10))
+#     plt.scatter(z_mean[:, 0], z_mean[:, 1], c=labels)
+#     plt.colorbar()
+#     plt.xlabel("z[0]")
+#     plt.ylabel("z[1]")
+#     plt.show()
+
+
+# (x_train, y_train), _ = keras.datasets.mnist.load_data()
+# x_train = np.expand_dims(x_train, -1).astype("float32") / 255
+
+# plot_label_clusters(vae, x_train, y_train)
 
