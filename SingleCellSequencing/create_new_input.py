87f54076cae7f63f72a2ff19c8ce859626ac867f
Emily Thomas <esthomas09@hotmail.com>
2021-09-03 11:07:16 +0100

    create_new_input

--- SingleCellSequencing/create_new_input.py
+++ SingleCellSequencing/create_new_input.py
@@ -31,62 +31,84 @@ pathway_m = []
 expr_n = []
 expr_m = []
 
+print(len(nucl["present"] == 1))
+nuc_coords = coord[coord["nucleus"] == 1]
+nuc_coords = nuc_coords[["x", "y", "z"]]
+mito_coords = coord[coord["mitochondrion"] == 1]
+mito_coords = mito_coords[["x", "y", "z"]]
+
+
 
 for name, p, m, n in zip(p_expr["pathway"][0:100], p_expr["x"][0:100], mito["present"], nucl["present"]):
     if n == 1 and m == 0:
         per = (p/total_nucl)*100
-        points = (total_nucl/100) * per
+        points = (len(nuc_coords)/100) * per
         pathway_n.append(name)
         expr_n.append(round(points))
     elif n == 0 and m == 1: 
         per = (p/total_mito)*100
-        points = (total_mito/100) * per
+        points = (len(mito_coords)/100) * per
         pathway_m.append(name)
         expr_m.append(round(points))
     elif n == 1 and m == 1:
         per = ((p/2)/total_mito)*100
-        points = (total_mito/100) * per
+        points = (len(mito_coords)/100) * per
         pathway_m.append(name)
         expr_m.append(round(points))
 
         per = ((p/2)/total_nucl)*100
-        points = (total_nucl/100) * per
+        points = (len(nuc_coords)/100) * per
         pathway_n.append(name)
         expr_n.append(round(points))
     else:
         print("not matched conditions")
 
-print(sum(expr_n), total_nucl)
+# print(sum(expr_n), total_nucl)
 
-print(len(expr_n))
-print(len(expr_m))
+# print(len(expr_n))
+# print(len(expr_m))
 
 nuc_coords = coord[coord["nucleus"] == 1]
 nuc_coords = nuc_coords[["x", "y", "z"]]
-print(nuc_coords)
+
 
 n_array = nuc_coords.to_numpy()
-print(n_array)
+
+nuc_coords["colour"] = np.zeros
+
+
 
 from sklearn.neighbors import KDTree
 
 # this is just for a dummy pathway pretending no. of points is 200
-# ideally i should really start  
-tree = KDTree(n_array)
-print(n_array[0])
-nearest_dist, nearest_ind = tree.query([n_array[0]], k=200)
+# ideally i should really start 
 
-print(nearest_ind[0])
+a = [231., 320.,  44.]
+print(a[0])
 
-nuc_coords["colour"] = np.zeros
-nuc_coords.loc[nearest_ind[0],'colour'] = 1
+print(nuc_coords[(nuc_coords['x'] == a[0])&(nuc_coords['y'] == a[1])&(nuc_coords['z'] == a[2])].index[0])
 
-new_n_array = np.delete(n_array, nearest_ind[0], 0)
+for i, j in zip(pathway_n, expr_n):  
+    
+    tree = KDTree(n_array)
+    nearest_dist, nearest_ind = tree.query([n_array[0]], k=j)    
+
+    coloured_array = np.take(n_array, nearest_ind[0], 0)
+    
+    indices = []
+    for c in coloured_array: 
+       index =  nuc_coords[(nuc_coords['x'] == c[0])&(nuc_coords['y'] == c[1])&(nuc_coords['z'] == c[2])].index[0]
+       indices.append(index) 
+    
+    n_array = np.delete(n_array, nearest_ind[0], 0)
+
+    nuc_coords.loc[indices,'colour'] = i
+    
 
 import plotly.express as px
 
 fig = px.scatter_3d(nuc_coords, x='x', y='y', z='z',
-              colour="colours")
+              color="colour")
 fig.show()
 
 
